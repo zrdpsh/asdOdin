74ebf424ee45257632f7d5b1a3a8ca0b2429e38c
zrdpsh <zerdposh@gmail.com>
2024-01-16 11:25:04 +0300

    extra class deleted

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+O
+LinkedList/LinkedListDraft.java,0\8\0801c11704b8a07734f88866fd2269425ad9a7f8
---
+++ LinkedList/LinkedListTest.java
@@ -0,0 +1,32 @@
+package LinkedList;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void removeAll() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void count() {
+    }
+
+    @Test
+    void insertAfter() {
+    }
+}
