ee380ea998bf59504eb185ddfc41afb4901dae65
zrdpsh <zerdposh@gmail.com>
2024-01-26 22:03:15 +0300

    class DynArray blueprint added

---
+++ DynArray/DynArray.java
@@ -0,0 +1,46 @@
+package DynArray;
+
+public class DynArray<T>
+{
+    public T [] array;
+    public int count;
+    public int capacity;
+    Class clazz;
+
+    public DynArray(Class clz)
+    {
+        clazz = clz; // нужен для безопасного приведения типов
+        // new DynArray<Integer>(Integer.class);
+
+        count = 0;
+        makeArray(16);
+    }
+
+    public void makeArray(int new_capacity)
+    {
+        // array = (T[]) Array.newInstance(this.clazz, new_capacity);
+        // ваш код
+    }
+
+    public T getItem(int index)
+    {
+        // ваш код
+        return null;
+    }
+
+    public void append(T itm)
+    {
+        // ваш код
+    }
+
+    public void insert(T itm, int index)
+    {
+        // ваш код
+    }
+
+    public void remove(int index)
+    {
+        // ваш код
+    }
+
+}
--- DynamicArray/DynamicArray.java
+++
@@ -1,4 +0,0 @@
-package DynamicArray;
-
-public class DynamicArray {
-}
