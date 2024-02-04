0f3f1d31ac4490d49b04e133ac75f21eb49e8db8
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2024-02-04 20:31:30 +0300

    Create Queue.java

---
+++ Queue/Queue.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Queue<T>
+{
+      public Queue()
+      {
+       // инициализация внутреннего хранилища очереди
+      } 
+
+      public void enqueue(T item)
+      {
+        // вставка в хвост
+      }
+
+      public T dequeue()
+      {
+        // выдача из головы
+        return null; // null если очередь пустая
+      }
+
+      public int size()
+      {
+        return 0; // размер очереди
+      }
+
+}
