f75ff75f86ed52f83da3f11f05be8927d02bf35c
zrdpsh <zerdposh@gmail.com>
2024-02-05 22:24:32 +0300

    Queue tests added

---
+++ Queue/QueueTest.java
@@ -0,0 +1,20 @@
+package Queue;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueueTest {
+
+    @Test
+    void enqueue() {
+    }
+
+    @Test
+    void dequeue() {
+    }
+
+    @Test
+    void size() {
+    }
+}
