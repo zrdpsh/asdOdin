026f53792fcade59c22b4c40c38ee9a7582b394e
zrdpsh <zerdposh@gmail.com>
2024-02-11 23:35:48 +0300

    Queue2Stacks tests added

---
+++ Stack/Queue2StacksTest.java
@@ -0,0 +1,20 @@
+package Stack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Queue2StacksTest {
+
+    @Test
+    void enqueue() {
+    }
+
+    @Test
+    void dequeue() {
+    }
+
+    @Test
+    void size() {
+    }
+}
