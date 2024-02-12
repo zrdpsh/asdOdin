250d722c937af7da2e02a8879aee313efdbf06d0
zrdpsh <zerdposh@gmail.com>
2024-02-13 00:18:24 +0300

    Deque methods added

--- Deque/Deque.java
+++ Deque/Deque.java
@@ -1,36 +1,53 @@
+package Deque;
+
 import java.util.*;
 
 public class Deque<T>
 {
+    LinkedList<T> lL;
+    T elem;
+    int len;
+
     public Deque()
     {
-     // инициализация внутреннего хранилища
+        lL = new LinkedList<>();
+        len = 0;
     }
 
     public void addFront(T item)
     {
-     // добавление в голову
+        lL.addFirst(item);
+        len++;
     }
 
     public void addTail(T item)
     {
-     // добавление в хвост
+        lL.addLast(item);
+        len++;
     }
 
     public T removeFront()
     {
-     // удаление из головы
-     return null;
+        if (len > 0) {
+            elem = lL.removeFirst();
+            len--;
+            return elem;
+        }
+        return null;
     }
 
     public T removeTail()
     {
-     // удаление из хвоста
-     return null;
+        if (len > 0) {
+            elem = lL.removeLast();
+            len--;
+            return elem;
+        }
+        return null;
     }
         
     public int size()
     {
-     return 0; // размер очереди
+     return len;
     }
 }
---
+++ Deque/DequeTest.java
@@ -0,0 +1,28 @@
+package Deque;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DequeTest {
+
+    @Test
+    void addFront() {
+    }
+
+    @Test
+    void addTail() {
+    }
+
+    @Test
+    void removeFront() {
+    }
+
+    @Test
+    void removeTail() {
+    }
+
+    @Test
+    void size() {
+    }
+}
