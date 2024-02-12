5c6fc4a9281fa06f8aa273fba9e75e4c6e811f6a
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2024-02-13 00:04:10 +0300

    Update Deque.java

--- Deque/Deque.java
+++ Deque/Deque.java
@@ -1,1 +1,36 @@
+import java.util.*;
 
+public class Deque<T>
+{
+    public Deque()
+    {
+     // инициализация внутреннего хранилища
+    }
+
+    public void addFront(T item)
+    {
+     // добавление в голову
+    }
+
+    public void addTail(T item)
+    {
+     // добавление в хвост
+    }
+
+    public T removeFront()
+    {
+     // удаление из головы
+     return null;
+    }
+
+    public T removeTail()
+    {
+     // удаление из хвоста
+     return null;
+    }
+        
+    public int size()
+    {
+     return 0; // размер очереди
+    }
+}
