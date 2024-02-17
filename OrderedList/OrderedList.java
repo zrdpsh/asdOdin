0c4331875a5d01cd81610ebb05935921d98345ee
zrdpsh <zerdposh@gmail.com>
2024-02-17 21:40:29 +0300

    tests added to OrderedList

--- OrderedList/OrderedList.java
+++ OrderedList/OrderedList.java
@@ -1,4 +1,5 @@
-import javax.print.DocFlavor;
+package OrderedList;
+
 import java.util.*;
 
 
@@ -33,6 +34,8 @@ import java.util.*;
     {
         int res = 0;
 
+        if (v1 == null || v2 == null) return res;
+
         if (v1.getClass() == Integer.class) res = ((Integer) v1).compareTo((Integer) v2);
 
         if (v1.getClass() == String.class) {
@@ -52,20 +55,84 @@ import java.util.*;
         Node<T> nd = new Node<>(value);
         if (head == null) {
             this.head = nd;
+            this.tail = nd;
+            len++;
+            return;
         }
 
+        Node<T> tmpNd = this.head;
+
+        if (_ascending && (compare(nd.value, tmpNd.value) < 0)) {
+            addInHead(nd, tmpNd);
+            return;
+        }
+
+        if (!_ascending && (compare(nd.value, tmpNd.value) > 0)) {
+            addInHead(nd, tmpNd);
+            return;
+        }
+
+        for (;tmpNd.next != null; tmpNd = tmpNd.next) {
+            if (_ascending && (compare(nd.value, tmpNd.value) < 0)) {
+                tmpNd.prev.next = nd;
+                nd.next = tmpNd;
+                len++;
+                return;
+            }
+
+            if (!_ascending && (compare(nd.value, tmpNd.value) > 0)) {
+                tmpNd.prev.next = nd;
+                nd.next = tmpNd;
+                len++;
+                return;
+            }
+        }
+
+        if (_ascending && (compare(nd.value, tmpNd.value) < 0)) {
+            addBeforeTail(nd, tmpNd);
+            return;
+        }
+
+        if (!_ascending && (compare(nd.value, tmpNd.value) > 0)) {
+            addBeforeTail(nd, tmpNd);
+            return;
+        }
+
+        nd.prev = tmpNd;
+        nd.prev.next = nd;
+        nd.next = null;
+        this.tail = nd;
+        len++;
+
     }
+
+    private void addInHead(Node<T> added, Node<T> moved) {
+        added.prev = null;
+        added.next = moved;
+        moved.prev = added;
+        this.head = added;
+        this.len++;
+    }
+
+      private void addBeforeTail(Node<T> added, Node<T> moved) {
+          added.prev = moved.prev;
+          added.prev.next = added;
+          added.next = moved;
+          moved.prev = added;
+          len++;
+      }
+
+//    private void addWithFlag
 
     public Node<T> find(T val)
     {
         Node<T> nd = this.head;
 
-        int compareRes = this.compare(nd.value, val);
-
         for (int i = 0; i < this.len; i++) {
+            int compareRes = this.compare(nd.value, val);
             if (compareRes == 0) return nd;
-            if (this._ascending && compareRes < 0) return null;
-            if ((!this._ascending) && compareRes > 0) return null;
+            if (this._ascending && compareRes > 0) return null;
+            if ((!this._ascending) && compareRes < 0) return null;
             nd = nd.next;
         }
         return null;
@@ -78,24 +145,27 @@ import java.util.*;
 
         if (this.head == this.tail) {
              this.clear(true);
+             len = len - (len > 0 ? 1 : 0);
              return;
         }
 
         if (nd.prev == null) {
             this.head = nd.next;
             nd.next.prev = null;
+            len = len - (len > 0 ? 1 : 0);
             return;
         }
 
         if (nd.next == null) {
             this.tail = nd.prev;
             nd.prev.next = null;
+            len = len - (len > 0 ? 1 : 0);
             return;
         }
 
         nd.prev.next = nd.next;
         nd.next.prev = nd.prev;
-
+        len = len - (len > 0 ? 1 : 0);
     }
 
     public void clear(boolean asc)
--- OrderedList/OrderedListTest.java
+++ OrderedList/OrderedListTest.java
@@ -1,38 +1,315 @@
 package OrderedList;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderedListTest {
 
     @Test
-    void compare() {
+    void compareNull() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+
+        int res = ol.compare(1, null);
+
+        Assertions.assertTrue(res == 0);
     }
 
     @Test
-    void add() {
+    void compareEqual() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+
+        int res = ol.compare(1, 1);
+
+        Assertions.assertTrue(res == 0);
     }
 
     @Test
-    void find() {
+    void compareGreater() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+
+        int res = ol.compare(2, 1);
+
+        Assertions.assertTrue(res == 1);
     }
 
     @Test
-    void delete() {
+    void compareLower() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+
+        int res = ol.compare(1, 2);
+
+        Assertions.assertTrue(res == -1);
+    }
+
+    @Test
+    void addToEmptyList() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+
+        ol.add(3007);
+
+        Assertions.assertTrue(ol.find(3007) != null);
+        Assertions.assertTrue(ol.count() == 1);
+    }
+
+    @Test
+    void addToHeadAsc() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+
+        ol.add(100);
+        ol.add(10);
+
+        Node<Integer> a = ol.find(10);
+
+        Assertions.assertTrue(a != null);
+        Assertions.assertTrue(a.next.value == 100);
+        Assertions.assertTrue(ol.count() == 2);
+    }
+
+    @Test
+    void addToHeadDesc() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(false);
+
+        ol.add(10);
+        ol.add(100);
+
+
+        Node<Integer> a = ol.find(100);
+
+        Assertions.assertTrue(a != null);
+        Assertions.assertTrue(a.next.value == 10);
+        Assertions.assertTrue(ol.count() == 2);
+    }
+
+    @Test
+    void addAsc() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+
+        ol.add(1);
+        ol.add(10);
+        ol.add(100);
+        ol.add(1000);
+
+        Node<Integer> a = ol.find(1);
+        Node<Integer> b = ol.find(10);
+        Node<Integer> c = ol.find(100);
+        Node<Integer> d = ol.find(1000);
+
+        Assertions.assertTrue(a != null);
+        Assertions.assertTrue(b != null);
+        Assertions.assertTrue(c != null);
+        Assertions.assertTrue(d != null);
+        Assertions.assertTrue(a.next.value == 10);
+        Assertions.assertTrue(b.next.value == 100);
+        Assertions.assertTrue(c.next.value == 1000);
+        Assertions.assertTrue(d.next == null);
+        Assertions.assertTrue(ol.count() == 4);
+    }
+
+    @Test
+    void addDesc() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(false);
+
+        ol.add(1);
+        ol.add(10);
+        ol.add(100);
+        ol.add(1000);
+
+        Node<Integer> a = ol.find(1);
+        Node<Integer> b = ol.find(10);
+        Node<Integer> c = ol.find(100);
+        Node<Integer> d = ol.find(1000);
+
+        Assertions.assertTrue(a != null);
+        Assertions.assertTrue(b != null);
+        Assertions.assertTrue(c != null);
+        Assertions.assertTrue(d != null);
+        Assertions.assertTrue(d.next.value == 100);
+        Assertions.assertTrue(c.next.value == 10);
+        Assertions.assertTrue(b.next.value == 1);
+        Assertions.assertTrue(a.next == null);
+        Assertions.assertTrue(ol.count() == 4);
+    }
+
+    @Test
+    void addInTailAsc() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+
+        ol.add(10);
+        ol.add(100);
+
+        Node<Integer> a = ol.find(100);
+
+        Assertions.assertTrue(a != null);
+        Assertions.assertTrue(a.prev.value == 10);
+        Assertions.assertTrue(ol.count() == 2);
+    }
+
+    @Test
+    void addInTailDesc() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(false);
+
+        ol.add(100);
+        ol.add(10);
+
+
+        Node<Integer> a = ol.find(10);
+
+        Assertions.assertTrue(a != null);
+        Assertions.assertTrue(a.prev.value == 100);
+        Assertions.assertTrue(ol.count() == 2);
+    }
+
+    @Test
+    void addBeforeTail() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+
+        ol.add(10);
+        ol.add(11);
+        ol.add(101);
+        ol.add(100);
+
+        Node<Integer> a = ol.find(100);
+
+        Assertions.assertTrue(a != null);
+        Assertions.assertTrue(a.prev.value == 11);
+        Assertions.assertTrue(ol.count() == 4);
+    }
+
+    @Test
+    void addInMiddle() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+
+        ol.add(10);
+        ol.add(100);
+        ol.add(11);
+
+        Node<Integer> a = ol.find(11);
+
+        Assertions.assertTrue(a != null);
+        Assertions.assertTrue(a.prev.value == 10);
+        Assertions.assertTrue(a.next.value == 100);
+        Assertions.assertTrue(ol.count() == 3);
+    }
+
+    @Test
+    void findInEmptyList() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+
+        Node<Integer> a = ol.find(100);
+
+        Assertions.assertTrue(a == null);
+        Assertions.assertTrue(ol.count() == 0);
+    }
+
+    @Test
+    void deleteFromEmptyList() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+
+        ol.delete(3007);
+
+        Assertions.assertTrue(ol.count() == 0);
+        Assertions.assertTrue(ol.head == null);
+        Assertions.assertTrue(ol.tail == null);
+    }
+
+    @Test
+    void deleteOne() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+
+        ol.add(3007);
+        ol.delete(3007);
+
+        Assertions.assertTrue(ol.count() == 0);
+        Assertions.assertTrue(ol.head == null);
+        Assertions.assertTrue(ol.tail == null);
+    }
+
+    @Test
+    void deleteFromHead() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+
+        ol.add(10);
+        ol.add(11);
+        ol.add(101);
+        ol.add(100);
+
+        ol.delete(10);
+
+        Assertions.assertTrue(ol.count() == 3);
+        Assertions.assertTrue(ol.head.value == 11);
+    }
+
+    @Test
+    void deleteFromTail() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+
+        ol.add(10);
+        ol.add(11);
+        ol.add(101);
+        ol.add(100);
+
+        ol.delete(101);
+
+        Assertions.assertTrue(ol.count() == 3);
+        Assertions.assertTrue(ol.tail.value == 100);
     }
 
     @Test
     void clear() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+
+        ol.add(10);
+        ol.add(100);
+        ol.add(11);
+
+        ol.clear(true);
+
+        Assertions.assertTrue(ol.count() == 0);
     }
 
     @Test
-    void count() {
-//        int res = -8 | 0;
-//        System.out.println(res);
+    void countNull() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+
+        Assertions.assertTrue(ol.count() == 0);
     }
 
     @Test
-    void getAll() {
+    void countRand() {
+        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+
+        Random rand = new Random();
+        int ri = rand.nextInt(10000);
+
+        for (int i = 0; i < ri; i++) ol.add(i);
+
+        Assertions.assertTrue(ol.count() == ri);
+        Assertions.assertTrue(ol.head.value == 0);
+        Assertions.assertTrue(ol.tail.value == (ri-1));
     }
+
+//    @Test
+//    void getAll() {
+//        OrderedList<Integer> ol = new OrderedList<Integer>(true);
+//
+//        ol.add(10);
+//        ol.add(100);
+//        ol.add(11);
+//
+//        ArrayList<Node<Integer>> a = ol.getAll();
+//
+//        ArrayList<Node<Integer>> b = new ArrayList<Node<Integer>>();
+//        b.add(new Node<>(10));
+//        b.add(new Node<>(11));
+//        b.add(new Node<>(110));
+//
+//
+//        Assertions.assertTrue(ol.count() == 3);
+//        Assertions.assertTrue(a.equals(b));
+//    }
 }
