14af2f2e7b0b7221e6ead0c13308f7edced5e95f
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2024-02-18 20:11:44 +0300

    Create Hashtable.java

---
+++ HashTable/Hashtable.java
@@ -0,0 +1,41 @@
+public class HashTable
+  {
+    public int size;
+    public int step;
+    public String [] slots; 
+
+    public HashTable(int sz, int stp)
+    {
+      size = sz;
+      step = stp;
+      slots = new String[size];
+      for(int i=0; i<size; i++) slots[i] = null;
+    }
+
+    public int hashFun(String value)
+    {    
+         // всегда возвращает корректный индекс слота
+         return 0;
+    }
+
+    public int seekSlot(String value)
+    {
+         // находит индекс пустого слота для значения, или -1
+         return -1;
+    }
+
+     public int put(String value)
+     {
+         // записываем значение по хэш-функции
+         
+         // возвращается индекс слота или -1
+         // если из-за коллизий элемент не удаётся разместить 
+         return -1;
+     }
+
+     public int find(String value)
+     {
+         // находит индекс слота со значением, или -1
+         return -1;
+     }
+  }
