984c73e3a65c3bdc171ab5dd9b2a8960570a1665
zrdpsh <zerdposh@gmail.com>
2024-02-19 22:03:22 +0300

    tests added for HashTable

---
+++ HashTable/HashTableTest.java
@@ -0,0 +1,24 @@
+package HashTable;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HashTableTest {
+
+    @Test
+    void hashFun() {
+    }
+
+    @Test
+    void seekSlot() {
+    }
+
+    @Test
+    void put() {
+    }
+
+    @Test
+    void find() {
+    }
+}
