9b59a5f32a9c964b411c359ac971edaf30c79222
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2024-03-01 18:00:05 +0300

    Create PowerSet.java

---
+++ PowerSet/PowerSet.java
@@ -0,0 +1,60 @@
+public class PowerSet
+{
+
+   public PowerSet()
+   {
+     // ваша реализация хранилища
+   }
+
+   public int size()
+   {
+        // количество элементов в множестве
+        return 0;
+   }
+
+
+   public void put(String value)
+   {
+     // всегда срабатывает
+   }
+
+    public boolean get(String value)
+    {
+        // возвращает true если value имеется в множестве,
+        // иначе false
+        return false;
+    }
+
+    public boolean remove(String value)
+    {
+        // возвращает true если value удалено
+        // иначе false
+        return false;
+    }
+
+    public PowerSet intersection(PowerSet set2)
+    {
+        // пересечение текущего множества и set2
+        return null;
+    }
+
+    public PowerSet union(PowerSet set2)
+    {
+        // объединение текущего множества и set2
+        return null;
+    }
+
+    public PowerSet difference(PowerSet set2)
+    {
+        // разница текущего множества и set2
+        return null;
+    }
+
+    public boolean isSubset(PowerSet set2)
+    {
+        // возвращает true, если set2 есть
+        // подмножество текущего множества,
+        // иначе false
+        return false;
+    }
+}
