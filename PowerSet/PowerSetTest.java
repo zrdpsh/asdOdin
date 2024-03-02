fb08ba1d53f39495ab529afad9b3d83471c5b14b
zrdpsh <zerdposh@gmail.com>
2024-03-02 20:32:19 +0300

    powerSet tests added

---
+++ PowerSet/PowerSetTest.java
@@ -0,0 +1,44 @@
+package PowerSet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PowerSetTest {
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void put() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void intersection() {
+    }
+
+    @Test
+    void union() {
+    }
+
+    @Test
+    void difference() {
+    }
+
+    @Test
+    void isSubset() {
+    }
+
+    @Test
+    void a() {
+    }
+}
