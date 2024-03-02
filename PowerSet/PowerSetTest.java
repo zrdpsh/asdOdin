5572284fd5c2bd5bbc0962f4afca4d732d4bf332
zrdpsh <zerdposh@gmail.com>
2024-03-03 01:56:01 +0300

    speedStes and isSubset tests added

--- PowerSet/PowerSetTest.java
+++ PowerSet/PowerSetTest.java
@@ -1,5 +1,6 @@
 package PowerSet;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -35,10 +36,39 @@ class PowerSetTest {
     }
 
     @Test
-    void isSubset() {
+    void isSubsetNull() {
+        PowerSet ps1 = new PowerSet();
+        PowerSet ps2 = new PowerSet();
+
+        Assertions.assertTrue(ps1.isSubset(ps2));
     }
 
     @Test
-    void a() {
+    void isSubsetDefault() {
+        PowerSet ps1 = new PowerSet();
+        PowerSet ps2 = new PowerSet();
+
+        for (int i = 0; i < 10; i++) {
+            ps1.put(String.valueOf(i));
+            ps2.put(String.valueOf(i));
+        }
+
+        Assertions.assertTrue(ps1.isSubset(ps2));
     }
+
+
+    @Test
+    void speedTest() {
+        PowerSet ps1 = new PowerSet();
+        PowerSet ps2 = new PowerSet();
+
+        for (int i = 0; i < 20000; i++) {
+            ps1.put(String.valueOf(i));
+            ps2.put(String.valueOf(i));
+        }
+
+        Assertions.assertTrue(ps1.isSubset(ps2));
+    }
+
+
 }
