a286dacb16efdef9d412d908b761ae4a239aeb0a
zrdpsh <zerdposh@gmail.com>
2024-03-07 06:07:47 +0300

    hash1 test added

--- BloomFilter/BloomFilterTest.java
+++ BloomFilter/BloomFilterTest.java
@@ -1,5 +1,6 @@
 package BloomFilter;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,7 +8,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class BloomFilterTest {
 
     @Test
-    void hash1() {
+    void hash1Default() {
+        BloomFilter bf = new BloomFilter(32);
+
+        int res = bf.hash1("0123456789");
+        int res2 = bf.hash1("1234567890");
+
+        Assertions.assertTrue(res == 13);
+        Assertions.assertTrue(res2 == 29);
     }
 
     @Test
