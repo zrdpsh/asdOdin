aa0e898f02b71c03768619cfdb02b79440cb33c5
zrdpsh <zerdposh@gmail.com>
2024-03-07 21:26:27 +0300

    add method updated

--- BloomFilter/BloomFilter.java
+++ BloomFilter/BloomFilter.java
@@ -4,46 +4,39 @@ public class BloomFilter
 {
     private static final int RAND1 = 17;
     private static final int RAND2 = 223;
+    int MASK = 0x0_000_0000;
+    int binaryString;
 
     public int filter_len;
 
       public BloomFilter(int f_len)
       {
         filter_len = f_len;
-        // создаём битовый массив длиной f_len ...
+        binaryString = MASK;
 
       }
 
-      // хэш-функции
       public int hash1(String str1)
       {
-          int rand = 17;
-        // 17
-        //for(int i=0; i<str1.length(); i++)
-        //{
-        //    int code = (int)str1.charAt(i);
-        //}
-        // реализация ...
-
-        return hF(str1, RAND1);
+          return hF(str1, RAND1);
       }
+
       public int hash2(String str1)
       {
-        // 223
-        // реализация ...
           return hF(str1, RAND2);
       }
 
       public void add(String str1)
       {
-        // добавляем строку str1 в фильтр
-          int i1 = hash1(str1);
-          int i2 = hash2(str1);
+          binaryString = hash1(str1) | binaryString;
+          binaryString = hash2(str1) | binaryString;
       }
 
       public boolean isValue(String str1)
       {
-        // проверка, имеется ли строка str1 в фильтре
+          int i1 = hash1(str1);
+          int i2 = hash2(str1);
+
         return false;
       }
 
@@ -53,6 +46,6 @@ public class BloomFilter
               sum = (sum*rand + str.charAt(i)) % filter_len;
           }
 
-          return sum;
+          return sum | MASK;
       }
 }
