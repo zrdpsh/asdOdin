5b9368a7520f2f9a0d1ec072374f757d7b4c5205
zrdpsh <zerdposh@gmail.com>
2024-03-09 17:49:51 +0300

    Native cache class updated

--- NativeCache/NativeCache.java
+++ NativeCache/NativeCache.java
@@ -4,5 +4,81 @@ class NativeCache<T>
     public String [] slots;
     public T [] values;
     public int [] hits;
+
+    int step = 3;
     // ...
+
+    public int hashFun(String value)
+    {
+        int sum = 0;
+        for (int i = 0; i < value.length(); i++){
+            sum += value.charAt(i);
+        }
+        return (sum % size);
+    }
+
+
+    public int seekSlot(String value)
+    {
+        int i;
+        i = hashFun(value);
+        i = checkIndex(i, value);
+        return i;
+    }
+
+    public int checkIndex(int i, String key) {
+        i = checkViaStep(i, key);
+        if (i >= size) i = checkEach(key);
+        if (i >= size) i = -1;
+        return i;
+    }
+
+    public int checkViaStep(int i, String key) {
+        for(;i < size && slots[i] != null; i+=step){
+            if(slots[i%size].equals(key)) break;
+        }
+        return i;
+    }
+
+    public int checkEach(String key) {
+        int i = 0;
+        for(; i < size && slots[i] != null; i++){
+            if(slots[i%size].equals(key)) break;
+        }
+        return i;
+    }
+
+    public boolean isKey(String key)
+    {
+        int slot = hashFun(key);
+        if (slots[slot] != null && slots[slot].equals(key)) return true;
+
+        for (slot = 0; slot < slots.length; slot++) {
+            if (slots[slot] != null && slots[slot].equals(key)) return true;
+        }
+
+        return false;
+    }
+
+    public int put(String value)
+    {
+        int i;
+        i = seekSlot(value);
+        if(i != -1) slots[i] = value;
+        return i;
+    }
+
+    public int find(String value)
+    {
+        int i = hashFun(value);
+        if(value.equals(slots[i]))
+            return i;
+
+        for (i = 0; i < size; i++)
+            if(value.equals(slots[i]))
+                return i;
+        return -1;
+    }
+
+
 }
